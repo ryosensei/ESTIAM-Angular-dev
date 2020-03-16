d6c253a21b9a1d50bf86419fba30ad2e71abd463
azzwolf <thom.zpi@gmail.com>
2020-03-16 16:33:39 +0100

    fin de premiere séance

--- E-commerce-azzwolf/src/app/app.module.ts
+++ E-commerce-azzwolf/src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProduitsComponent } from './produits/produits.component';
+import { CategorieComponent } from './categorie/categorie.component';
+
+const appRoutes: Routes = [
+  {path:  'produits', component: ProduitsComponent },
+  {path:  'categorie', component: CategorieComponent },
+  {path:  '**', component: CategorieComponent },
+  
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProduitsComponent,
+    CategorieComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } 
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
